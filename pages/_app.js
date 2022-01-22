247e99c0e6c8837bb0f17e27565b4ae133a4344d
soheb alam <sohebalam@hotmail.com>
2022-01-22 11:34:16 +0000

    googleauth

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import { SessionProvider } from "next-auth/react"
+
+function App({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
+
+export default App
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import GoogleProvider from "next-auth/providers/google"
+import clientPromise from "../../../lib/mongodb"
+
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+})
